5636e5277f85ab0ed7688e6da79b153359d65ad6
arpitha33 <arpithashetty262@gmail.com>
2024-11-22 09:28:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Menu from './components/Menu';
+
+import Cart from './components/Cart';
+import About from './components/About';
+import Auth from './components/Auth';
+
+import Footer from './components/Footer';
+import Feedback from './components/Feedback';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+         
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/feedback" element={<Feedback />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
