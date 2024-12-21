00a2560c04d6b49c6ea96b7fcf8eb0916efed58a
arpitha33 <arpithashetty262@gmail.com>
2024-12-21 21:10:24 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer style={{height: '50px', display: 'flex', alignItems:"center", justifyContent: "center", marginRight: "10px", color: "#fff"}}>
       <p>&copy; 2023 Foodie Zone. All rights reserved.</p>
       <a href="#feedback">Give Feedback</a>
     </footer>
